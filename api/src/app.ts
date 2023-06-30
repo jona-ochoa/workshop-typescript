75908b8430eeb7702c0fc88111cd7c6bcd93dae1
jochoa <jonatan.c.ochoa@gmail.com>
2023-06-30 18:22:21 -0300

    first commit

---
+++ api/src/app.ts
@@ -0,0 +1,37 @@
+import express, {Request, Response, NextFunction} from 'express';
+import routes from './routes/index'
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import cors from "cors";
+import config from './lib/config';
+
+const app = express();
+app.use(express.urlencoded({extended: true, limit: '50mb'})); //middleware
+app.use(express.json({limit: '50mb'}));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(
+ cors({
+  origin: config.cors,
+  credentials: true,
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+ })
+);
+
+interface error {
+    status: number;
+    message: string;
+   }
+   app.use((err: error, req: Request, res: Response, next: NextFunction) => {
+ // eslint-disable-line no-unused-vars
+ const status = err.status || 500;
+ const message = err.message || err;
+ console.error(err);
+ res.status(status).send(message);
+});
+
+app.use('/api', routes)
+
+export default app;
---
+++ src/redux/reducer/reducer.ts
@@ -0,0 +1,7 @@
+const initialState = {
+    counter: 1,
+};
+
+export default function reducer(state = initialState, action) {
+    return state;
+}
